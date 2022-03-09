3a2374c41154fdf0be9f7293892a268e05a78c5e
Dogukan Ozcan <dogukankerem@hotmail.com>
2022-03-09 17:37:11 +0300

    Add Js and bg

---
+++ script.js
@@ -0,0 +1,62 @@
+
+const form = document.querySelector('form');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.querySelector('#btnDeleteAll');
+const taskList = document.querySelector('#task-list');
+
+
+eventListeners();
+
+function eventListeners(){
+ 
+    form.addEventListener('submit',addNewItem);
+    taskList.addEventListener('click', deleteItem);
+    btnDeleteAll.addEventListener('click', deleteAllItems);
+}
+
+function addNewItem(e){
+    if(input.value ===''){
+        alert('Please New İtem');
+    }
+
+    const li= document.createElement('li');
+    li.className='list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(input.value));
+
+    const a =document.createElement('a');
+    a.classList='delete-item float-right';
+    a.setAttribute('href','#');
+    a.innerHTML='<i class="fas fa-times"></i>';
+
+    li.appendChild(a);
+
+    taskList.appendChild(li);
+
+    input.value='';
+
+    e.preventDefault();
+
+}
+
+function deleteItem(e) {
+    
+
+        if (e.target.className === 'fas fa-times') {
+            if (confirm('Sure ?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
+}
+function deleteAllItems(e) {
+
+    if (confirm('Sure ?')) {
+        // taskList.innerHTML='';
+        taskList.childNodes.forEach(function (item) {
+            if (item.nodeType === 1) {
+                item.remove();
+            }
+        });
+    }
+    e.preventDefault();
+}
